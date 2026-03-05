d4d79face39518dba3080ebba29beaccbe0ca346
Dima Shchennikov <shchennikovdp@yandex.ru>
2026-03-05 22:05:41 +0300

    add media 1440px

--- gulpfile.js
+++ gulpfile.js
@@ -4,36 +4,35 @@ const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
-
 function html() {
-  return gulp.src('src/**/*.html')
-        .pipe(plumber())
-                .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/**/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 exports.html = html;
 
-
 function css() {
-  return gulp.src('src/styles/**/*.css')
-        .pipe(plumber())
-        .pipe(concat('bundle.css'))
-                .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/styles/**/*.css')
+    .pipe(plumber())
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 exports.css = css;
 
-
 function images() {
-  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-            .pipe(gulp.dest('dist/images'))
-            .pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 exports.images = images;
-
 
 function clean() {
   return del('dist');
@@ -41,11 +40,9 @@ function clean() {
 
 exports.clean = clean;
 
-
 const build = gulp.series(clean, gulp.parallel(html, css, images));
 
 exports.build = build;
-
 
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
@@ -56,11 +53,10 @@ const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.watchapp = watchapp;
 
-
 function serve() {
   browserSync.init({
     server: {
-      baseDir: './dist'
-    }
+      baseDir: './dist',
+    },
   });
 }
